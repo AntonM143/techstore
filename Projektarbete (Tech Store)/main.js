48a43164cfa0d10df7ff9f948dc72cb3e6e99aba
Joakim <j-pf@live.se>
2020-10-19 09:48:25 +0200

    changed Cart function added button but no function

--- Projektarbete (Tech Store)/main.js
+++ Projektarbete (Tech Store)/main.js
@@ -16,59 +16,112 @@ function loadProducts() {
 function initSite() {
     loadProducts();
     // This would also be a good place to initialize other parts of the UI
+}
 
+//Create Title Element and get content
+function getTitleElement(product){ 
+    productTitle =document.createElement("h2")
+    productTitle.className = "productTitle"
+    productTitle.innerText = product.title
+    return productTitle
+}
 
+//Create Price element and get content
+function getPriceElement(product){ 
+    productPrice = document.createElement("p")
+    productPrice.className = "productPrice"
+    productPrice.innerText = product.price + ":-"
+    return productPrice
+}
+
+//Create Description element and get content
+function getDescriptionElement(product){ 
+    productDescr = document.createElement("div")
+    productDescr.className = "productDescr"
+    productDescr.innerText = product.description
+    return productDescr
+}
+
+// Create IMG element append to parent container div and get source
+function getImgElement(product){ 
+    productImgContainer = document.createElement("div")
+    productImgContainer.className = "productImgContainer" 
+    productImg = document.createElement("img")
+    productImg.className = "productImg"
+    productImg.src = listOfProducts[i].image
+    productImgContainer.appendChild(productImg)
+    return productImgContainer
+}
+
+//Create productCard and append it to productContainer
+function createProductCard() {
+    productCard = document.createElement("div")
+    productCard.className = "productCard" 
+    productContainer.appendChild(productCard)
+    return productCard
+}
+
+// Create a button 
+function cartButton() {
+    productButton = document.createElement("button")
+    productButton.className = "productButton"
+    productButton.innerText = "lägg till i kundvagn"
+    
+    productButton.addEventListener("click", function(product){
+        let savedLocalProducts = localStorage.getItem("localProducts") //Hämta localstorage som e sparade under "localProducts"
+
+        if(savedLocalProducts == null){ // kolla om det finns något i localProducts, om inte skapa en array
+            savedLocalProducts = []
+            console.log("i If")
+        }
+        else { // Om det finns något i localProducts hämta det och parsea det
+            savedLocalProducts = JSON.parse(savedLocalProducts)
+            console.log("i Else")
+        }
+        savedLocalProducts.push({   //Pushar och skapar mitt object in i min array vid namn "savedLocalData"
+        title: getTitleElement(product).data
+
+        
+/*         productTitle.innerText
+ */
+    
+    })
+    console.log(savedLocalProducts)
+})
+    
+    
+    return productButton
 }
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
-    // Check your console to see that the products are stored in the listOfProducts varible.
-   
+
     for(i = 0; i < listOfProducts.length; i++){
         
-        //Skapar html för våra produkt kort
-        let productTitle =document.createElement("h2")
-        let productImg = document.createElement("img")
-        let productImgContainer = document.createElement("div")
-        let productDescr = document.createElement("div")
-        let productPrice = document.createElement("p")
-        let productCard = document.createElement("div")
-        
-        
-        
-        //Hämtar kontent för produkter från vår JSON fil
-        productImg.src = listOfProducts[i].image
-        productTitle.innerText = listOfProducts[i].title
-        productDescr.innerText = listOfProducts[i].description
-        productPrice.innerText = listOfProducts[i].price
-        
-        //Lägger till våra produkter till vår sida
-        productContainer.appendChild(productCard)
-        productCard.appendChild(productTitle)
-        productCard.appendChild(productDescr)
-        productImgContainer.appendChild(productImg)
-        productCard.appendChild(productImgContainer) 
-        productCard.appendChild(productPrice)
-        
-        //ger våra produkt "properties" klass namn så vi kan styla dem i css
-        productCard.className = "productCard"
-        productPrice.className = "productPrice"
-        productDescr.className = "productDescr"
-        productImgContainer.className = "productImgContainer"
-        productImg.className = "productImg"
-        productTitle.className = "productTitle"
 
-        //All styling ska flytta till css, men kan läggas här under utveckling
+        createProductCard()
+        cartButton()
 
+        productCard.appendChild(getTitleElement(listOfProducts[i]))
+        productCard.appendChild(getDescriptionElement(listOfProducts[i]))
+        productCard.appendChild(getImgElement(listOfProducts[i])) 
+        productCard.appendChild(getPriceElement(listOfProducts[i]))
+        productCard.appendChild(productButton)
     }
-    
-    console.log(listOfProducts);
-    
-    // Add your code here, remember to brake your code in to smaller function blocks
-    // to reduce complexity and increase readability. Each function should have
-    // an explainetory comment like the one for this function, see row 22.
-    
-    // TODO: Remove the console.log and these comments when you've read them.
+
 }
 
+/* function getCartButton(product) {
+    let productButton = document.createElement("button")
+    productContainer.appendChild(productButton) // for developing, change to correct container once done
+    productButton.className = "productButton"
+    productButton.innerText = "lägg till i kundvagn"
+    productButton.data = JSON.stringify(product)
+    productButton.addEventListener("click", function(bla){
 
+        console.log(bla.target.data)
+    })
+}
+
+  getCartButton({test:1})  */
+ 
