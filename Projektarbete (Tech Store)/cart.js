0861ee02e4dde25f721009ef6a3ebd9f92121437
Anton Månsson <anton.mansson@mediainstitutet.se>
2020-11-05 14:38:42 +0100

    Skriver ut totalbelopp på cart sidan

--- Projektarbete (Tech Store)/cart.js
+++ Projektarbete (Tech Store)/cart.js
@@ -1,10 +1,12 @@
+function initSite(){
+
+}
 function allPrices() {
 	let userList = localStorage.getItem("users")
 	let activeUser = sessionStorage.getItem("customer")
 	let totalt = 0
 	userList = JSON.parse(userList)
 	
-
 	if(activeUser !== null){
 		console.log("active user exist, and therefor userlist exists")
 		for(i = 0; i < userList.length; i++){	
@@ -14,9 +16,13 @@ function allPrices() {
 				cart = userList[i].cart
  					for(i = 0; i < cart.length; i++){
 						let price = cart[i].price
+						let totalSumCon = document.getElementById("productContainer")
+						let totalSum = document.createElement("p")
+						totalSumCon.innerHTML = ""
 						totalt += price
-						console.log("price = ", price)
-						console.log("totalt = ", totalt)
+						totalSum.className = "totalSum";
+						totalSum.innerText += "Totalt pris: " + totalt + " kr";
+						totalSumCon.appendChild(totalSum)
 					}
 					return
 			}
@@ -27,5 +33,4 @@ function allPrices() {
         //Möjglitvis skapa en funktion som räknar priser på produkter som inte är sparade till användare
 	}
  }
-
  allPrices()
