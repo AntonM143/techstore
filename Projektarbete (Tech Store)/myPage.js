909c750af0719e8891ea97257a55aa2804ff4ca7
robinhlgrsn <robinholgersson91@gmail.com>
2020-11-16 10:03:10 +0100

    lods product on mypage

--- Projektarbete (Tech Store)/myPage.js
+++ Projektarbete (Tech Store)/myPage.js
@@ -1,5 +1,7 @@
-import {loginLogoutButton, cartCounter} from "./main.js"
+import {loginLogoutButton, cartCounter,createProductCard,getImgElement,getTitleElement,getPriceElement} from "./main.js"
+import {parseUserList} from "./cart.js"
 let body = document.getElementById("myPageBody")
+let activeUser = sessionStorage.getItem("customer")
 window.addEventListener("load", initSite)
 
 function initSite() {
@@ -9,12 +11,36 @@ function initSite() {
 		cartCounter()
 		orderHistory()
 		
+		
 	}
 }
 	
 function orderHistory(){
 	
+	
+	let userList = parseUserList()
+	let cartDiv = document.getElementById("cartProducts")
+	
+		for (let i = 0; i < userList.length; i++){
 
+			if(activeUser == userList[i].customer) {
 
+			let currentUser = userList[i]
+			let pastOrders = currentUser.oldOrders
+				
+				for(let i = 0; i < pastOrders.length; i++){
+					
+					let productCard = createProductCard("cartProdCard", cartDiv)
+					let inOrders = pastOrders[i]
 
-	}
+					for (let i = 0; i < inOrders.length; i++ ){
+						productCard.appendChild(getImgElement(inOrders[i], "cartImg"))
+						productCard.appendChild(getTitleElement(inOrders[i]))
+						productCard.appendChild(getPriceElement(inOrders[i]))
+
+					}
+				}
+			}
+
+		}		
+}		
